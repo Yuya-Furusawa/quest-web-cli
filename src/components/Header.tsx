fb11c1c5f99b67de64717ad0266add107f83a5fe
Yuya Furusawa <fullyou0798@gmail.com>
2023-02-01 17:41:00 +0900

    Buttonの実装

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,19 @@
 import * as React from "react";
 
+import Button from "./Button";
+
 const Header = () => (
   <div className="flex justify-center w-full h-16 shadow">
-    <div className="w-2/5 text-sky-600 font-bold leading-[4rem]">
-      Quest Web App
+    <div className="flex justify-between items-center w-2/5">
+      <div className="text-sky-600 font-bold leading-[4rem]">Quest Web App</div>
+      <div className="flex gap-x-3">
+        <Button
+          text="SignUp"
+          primary={false}
+          onClick={() => console.log("onClick!")}
+        />
+        <Button text="Login" primary onClick={() => console.log("onClick!")} />
+      </div>
     </div>
   </div>
 );
