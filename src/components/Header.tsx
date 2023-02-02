61c7dab5b3f9022752143fe79b406b91a3f25b8b
Yuya Furusawa <fullyou0798@gmail.com>
2023-02-03 02:51:59 +0900

    ButtonをNavButtonに

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,14 @@
 import * as React from "react";
 
-import Button from "./Button";
+import NavButton from "./NavButton";
 
 const Header = () => (
   <div className="flex justify-center w-full h-16 shadow">
     <div className="flex justify-between items-center w-2/5">
       <div className="text-sky-600 font-bold leading-[4rem]">Quest Web App</div>
       <div className="flex gap-x-3">
-        <Button
-          text="SignUp"
-          primary={false}
-          onClick={() => console.log("onClick!")}
-        />
-        <Button text="Login" primary onClick={() => console.log("onClick!")} />
+        <NavButton text="SignUp" primary={false} dest="/" />
+        <NavButton text="Login" primary dest="/login" />
       </div>
     </div>
   </div>
