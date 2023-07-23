183bafcabbe08ac297b3ab71ebe0b50a210fdd85
t13hayashi <pmdag6@gmail.com>
2023-07-23 16:31:39 +0900

    fix component

--- src/components/Quest/ChallengeCard.tsx
+++ src/components/Quest/ChallengeCard.tsx
@@ -9,26 +9,25 @@ type Props = {
   challenge: Challenge;
 };
 
-const ChallengeCard: React.FC<Props> = React.memo(({ challenge }) => {
-
-  // TODO: サーバーから返される完了ステータスに置き換える
-  const isCompleted = React.useMemo(() => challenge.id[0].toUpperCase() <= 'M', [challenge.id]);
-
-  return (
-    <Link to={`/challenge/${challenge.id}`}>
-      <div className="flex flex-col w-full hover:opacity-60 cursor-pointer">
-        {/* TODO: 一旦静的画像で代替 */}
-        <img
-          src={TokyoTowerColored}
-          alt="東京タワーのカラーアイコン"
-          className={'aspect-square object-cover ' + (isCompleted ? '' : 'grayscale')}
-        />
-        <Spacer size="8px" />
-        <div className="text-base text-center font-bold leading-normal line-clamp-1">{challenge.name}</div>
+const ChallengeCard: React.FC<Props> = React.memo(({ challenge }) => (
+  <Link to={`/challenge/${challenge.id}`}>
+    <div className="flex flex-col w-full hover:opacity-60 cursor-pointer">
+      {/* TODO: 一旦静的画像で代替 */}
+      <img
+        src={TokyoTowerColored}
+        alt="東京タワーのカラーアイコン"
+        className={
+          "aspect-square object-cover " +
+          (challenge.id[0].toUpperCase() <= "M" ? "" : "grayscale") // TODO: サーバーから返される完了ステータスに置き換える
+        }
+      />
+      <Spacer size="8px" />
+      <div className="text-base text-center font-bold leading-normal line-clamp-1">
+        {challenge.name}
       </div>
-    </Link>
-  );
-});
+    </div>
+  </Link>
+));
 
 ChallengeCard.displayName = "ChallengeCard";
 
