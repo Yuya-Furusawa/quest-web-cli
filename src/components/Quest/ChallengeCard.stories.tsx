d1582ea6f4b4913da82f36a48f249dad7cf76f2d
Yuya Furusawa <fullyou0798@gmail.com>
2023-08-15 23:30:50 +0900

    ストーリーでちゃんと画像を表示

--- src/components/Quest/ChallengeCard.stories.tsx
+++ src/components/Quest/ChallengeCard.stories.tsx
@@ -29,8 +29,10 @@ const challenge: Challenge = {
   latitude: 35.6895,
   longitude: 139.6917,
   stamp_name: "test",
-  stamp_color_image_url: "test-url-color",
-  stamp_gray_image_url: "test-url-monoclo",
+  stamp_color_image_url:
+    "https://res.cloudinary.com/dbbtfr2i5/image/upload/v1692109657/tokyo-tower_color_bofufc.png",
+  stamp_gray_image_url:
+    "https://res.cloudinary.com/dbbtfr2i5/image/upload/v1692109656/tokyo-tower_monoclo_jxf7fb.png",
   flavor_text: "test-text",
 };
 
