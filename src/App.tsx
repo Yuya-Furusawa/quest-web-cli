6d7e779aaa850e060260f2929df23fb3a72ffc52
Yuya Furusawa <fullyou0798@gmail.com>
2023-08-17 00:16:45 +0900

    Suspenseを用いる

--- src/App.tsx
+++ src/App.tsx
@@ -10,18 +10,21 @@ import Header from "./components/Header";
 import QuestPage from "./pages/QuestPage";
 import ProfilePage from "./pages/ProfilePage";
 import ChallengePage from "./pages/ChallengePage";
+import Loading from "@components/organisms/Loading";
 
 function App() {
   return (
     <AuthContextProvider>
       <Header />
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/quest/:id" element={<QuestPage />} />
-        <Route path="/challenge/:id" element={<ChallengePage />} />
-        <Route path="/profile" element={<ProfilePage />} />
+        <React.Suspense fallback={<Loading />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/quest/:id" element={<QuestPage />} />
+          <Route path="/challenge/:id" element={<ChallengePage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+        </React.Suspense>
       </Routes>
     </AuthContextProvider>
   );
---
+++ src/components/organisms/Loading.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+const Loading: React.FC = () => (
+  <div className="flex justify-center w-full">
+    <div className="w-2/5">Failed to load</div>
+  </div>
+);
+
+export default Loading;
