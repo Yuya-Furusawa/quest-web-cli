c181e0493a4be2d299edf334bb5cee15a5251742
Yuya Furusawa <fullyou0798@gmail.com>
2023-10-19 17:21:48 +0900

    ContextProviderの配置

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { Routes, Route } from "react-router-dom";
 
 import { AuthContextProvider } from "@context/auth";
+import { ActivityContextProvider } from "@context/activity";
 
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
@@ -14,15 +15,17 @@ import ChallengePage from "./pages/ChallengePage";
 function App() {
   return (
     <AuthContextProvider>
-      <Header />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/quest/:id" element={<QuestPage />} />
-        <Route path="/challenge/:id" element={<ChallengePage />} />
-        <Route path="/profile" element={<ProfilePage />} />
-      </Routes>
+      <ActivityContextProvider>
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/quest/:id" element={<QuestPage />} />
+          <Route path="/challenge/:id" element={<ChallengePage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+        </Routes>
+      </ActivityContextProvider>
     </AuthContextProvider>
   );
 }
